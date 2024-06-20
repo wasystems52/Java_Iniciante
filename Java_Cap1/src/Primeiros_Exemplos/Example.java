94fd80c13a4b69f8a3f3f4e59ac64989c8b55ea5
wasystems52 <wlbds43@gmail.com>
2024-06-19 22:16:58 -0300

    Projeto Iniciante criado

---
+++ Java_Cap1/src/Primeiros_Exemplos/Example.java
@@ -0,0 +1,12 @@
+package Primeiros_Exemplos;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Seja Bem Vindo ao Java");
+		
+	}
+
+}
